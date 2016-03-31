365cde180364b607bf2639486268cfd32da03c2d
Phonicavi <mardpazin7san@hotmail.com>
2016-03-31 10:56:51 +0800

    need privilege

--- src/runner.py
+++ src/runner.py
@@ -10,28 +10,39 @@ if __name__ == "__main__":
         client = Client()
         client.init(token)
 
-        url1='/api/macro/getChinaDataGDP.csv?field=&indicID=M010000002&indicName=&beginDate=&endDate='
-        code, result = client.getData(url1)
-        if code==200:
-            print result
-        else:
-            print code
-            print result
+        # url1='/api/macro/getChinaDataGDP.csv?field=&indicID=M010000002&indicName=&beginDate=&endDate='
+        # code, result = client.getData(url1)
+        # if code==200:
+        #     print result
+        # else:
+        #     print code
+        #     print result
+        #
+        # url2='/api/subject/getThemesContent.csv?field=&themeID=&themeName=&isMain=1&themeSource='
+        # code, result = client.getData(url2)
+        # if(code==200):
+        #     file_object = open('themes.csv', 'w')
+        #     file_object.write(result)
+        #     file_object.close()
+        # else:
+        #     print code
+        #     print result
+        #
+        # url3='/api/market/getMktEqud.csv?field=&beginDate=&endDate=&secID=&ticker=&tradeDate=20160328'
+        # code, result = client.getData(url3)
+        # if code==200:
+        #     file_object = open('mktequd20160328.csv', 'w')
+        #     file_object.write(result)
+        #     file_object.close()
+        # else:
+        #     print code
+        #     print result
 
-        url2='/api/subject/getThemesContent.csv?field=&themeID=&themeName=&isMain=1&themeSource='
-        code, result = client.getData(url2)
-        if(code==200):
-            file_object = open('themes.csv', 'w')
-            file_object.write(result)
-            file_object.close()
-        else:
-            print code
-            print result
-
-        url3='/api/market/getMktEqud.csv?field=&beginDate=&endDate=&secID=&ticker=&tradeDate=20160205'
-        code, result = client.getData(url3)
+        url4='/api/market/getBarRTIntraDay.json?securityID=600050.XSHG&startTime=&endTime=&unit=1'
+        # url4='/api/market/getBarRTIntraDayOneMinute.csv?time=11:20&exchangeCD=&unit=1'
+        code, result = client.getData(url4)
         if code==200:
-            file_object = open('mktequd20160205.csv', 'w')
+            file_object = open('oneminute20160330.csv', 'w')
             file_object.write(result)
             file_object.close()
         else:
